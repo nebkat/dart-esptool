5f4a2a44f014548989bf962e105d99514601d578
Nebojša Cvetković <nebkat@gmail.com>
2025-06-24 18:18:54 -0400

    test: More firmware fields tested

--- test/test_firmwares.dart
+++ test/test_firmwares.dart
@@ -11,14 +11,22 @@ void main() {
         appRequired: true,
       );
       print(image);
-      expect(image.appDescription!.projectName, 'hello_world');
-      expect(image.appDescription!.version, 'v5.3.1-10-ga97660f327');
-      expect(image.appDescription!.idfVersion, 'v5.3.1-10-ga97660f327');
-      expect(image.appDescription!.secureVersion, 0);
+      expect(image.header.chipId, ChipId.esp32s3);
+      expect(image.header.segmentCount, 5);
+      expect(image.header.spiMode, ImageSpiMode.dio);
+      expect(image.header.spiFrequency, ImageSpiFrequency.div1);
+      expect(image.header.spiSize, ImageFlashSize.mb2);
+      expect(image.header.hashAppended, true);
+
       expect(
         image.digest.map((i) => i.toRadixString(16).padLeft(2, "0")).join(''),
         '65a8b80507138ea1797e3e2f503fe514f0f896ba617b5808fa9294f9dcdb868c',
       );
+
+      expect(image.appDescription!.projectName, 'hello_world');
+      expect(image.appDescription!.version, 'v5.3.1-10-ga97660f327');
+      expect(image.appDescription!.idfVersion, 'v5.3.1-10-ga97660f327');
+      expect(image.appDescription!.secureVersion, 0);
     });
   });
 }
